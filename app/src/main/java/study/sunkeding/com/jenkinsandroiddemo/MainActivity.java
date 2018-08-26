99243f6222787f58811fd6978f8f0b46b33a208a
keding <keding@leoao.com>
2018-08-26 20:30:33 +0800

    新增个toast

--- app/src/main/java/study/sunkeding/com/jenkinsandroiddemo/MainActivity.java
+++ app/src/main/java/study/sunkeding/com/jenkinsandroiddemo/MainActivity.java
@@ -2,6 +2,7 @@ package study.sunkeding.com.jenkinsandroiddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO: 2018/8/26 jenkins上会不会有代码提交记录 
+        // TODO: 2018/8/26 jenkins上会不会有代码提交记录
+        Toast.makeText(this, "9999", Toast.LENGTH_SHORT).show();
                 
     }
 }
