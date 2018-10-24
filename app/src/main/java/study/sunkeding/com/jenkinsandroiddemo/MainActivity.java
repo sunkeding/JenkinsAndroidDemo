a510389855b69fe413bb25244e3e522d5acb0e1a
keding <keding@leoao.com>
2018-10-24 16:08:04 +0800

    加个toast加个我是master的toast

--- app/src/main/java/study/sunkeding/com/jenkinsandroiddemo/MainActivity.java
+++ app/src/main/java/study/sunkeding/com/jenkinsandroiddemo/MainActivity.java
@@ -16,12 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // TODO: 2018/8/26 jenkins上会不会有代码提交记录
-        Toast.makeText(this, "9999", Toast.LENGTH_SHORT).show();
-        // TODO: 2018/8/27 提交记录加个todo
-
-        TextView textView=null;
-        textView.setText("dsa");
-        String substring = "9999".substring(0, 10);
+        Toast.makeText(this, "我是master分支", Toast.LENGTH_SHORT).show();
     }
 }
